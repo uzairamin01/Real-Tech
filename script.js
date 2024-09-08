dec91160694c01de02647089feb2bfde3968e61a
uzairamin01 <uzairamin476@gmail.com>
2024-09-08 15:32:10 -0700

    create responsive

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-const close = document.getElementById('close');
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const close = document.getElementById("close");
 
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('show');
-  hamburger.classList.toggle('toggle');
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+  hamburger.classList.toggle("toggle");
 });
 
-close.addEventListener('click', () => {
-  menu.classList.remove('show');
-  hamburger.classList.remove('toggle');
+close.addEventListener("click", () => {
+  menu.classList.remove("show");
+  hamburger.classList.remove("toggle");
 });
-
