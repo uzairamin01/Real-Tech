bdff71b55060df72c146e16fb434a5f0a83287b4
uzairamin01 <uzairamin476@gmail.com>
2024-09-10 15:12:07 -0700

    checking

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const hamburger = document.getElementById("hamburger");
+onload();
+
+
+function onload() {
+  menuSideBar();
+  animationForMobile();
+}
+
+function menuSideBar(){
+  const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 const close = document.getElementById("close");
 
@@ -11,3 +20,41 @@ close.addEventListener("click", () => {
   menu.classList.remove("show");
   hamburger.classList.remove("toggle");
 });
+}
+
+function animationForMobile() {
+  let mm = gsap.matchMedia();
+  let tl = gsap.timeline();
+  mm.add("(max-width:1023px)", ()=>{
+
+    gsap.from(".logo",  {
+      x: -1000,
+      opacity: 0,
+      duration: 1,
+    })
+    gsap.from("#hamburger",  {
+      x: 1000,
+      opacity: 0,
+      duration: 1.2,
+    })
+    gsap.from(".textarea h1, .textarea h2, .textarea p,.textarea button, .img img", {
+      opacity: 0,
+      y: 50,
+      stagger: 0.2,   // 0.2 seconds delay between each animation
+      duration: 1     // Each animation lasts 1 second
+    });
+
+    gsap.from(".pictures img, .pictures h1", {
+      y: 100,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".second-page",
+      }
+    })
+
+  })
+}
+
+
