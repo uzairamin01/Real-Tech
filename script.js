d52458b83ca8ba54b63709b14cbfc5648e9548b9
uzairamin01 <uzairamin476@gmail.com>
2024-09-10 16:28:31 -0700

    checking

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ onload();
 function onload() {
   menuSideBar();
   animationForMobile();
+  dekstopAnimation();
 }
 
 function menuSideBar(){
@@ -60,6 +61,61 @@ function animationForMobile() {
     //   }
     // })
 
+  })
+}
+
+function dekstopAnimation(){
+  let mm = gsap.matchMedia();
+  mm.add("(min-width:1024px)", ()=>{
+    gsap.from(".logo, .desktop-menu a",{
+      y: -100,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.2,
+    });
+    gsap.from(".textarea h1, .textarea h2, .textarea p, .textarea button", {
+      x: -100,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.2,
+    })
+    gsap.from(".img img", {
+      x: 100,
+      y: 100,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.2,
+    })
+    gsap.from(".pictures img, .pictures h1",{
+      y: 50,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".pictures",
+        start: "top 60%"
+      }
+    })
+    gsap.from(".textsarea h1, .textsarea p, .textsarea button, .textsarea i", {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".textsarea",
+        start: "top 40%",
+      }
+    })
+    gsap.from(".textarea-fourth h1, .textarea-fourth p, .textarea-fourth button", {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.2,
+      scrollTrigger: {
+        trigger: ".textarea-fourth",
+        start: "top 50%",
+      }
+    })
   })
 }
 
